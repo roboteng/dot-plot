6e7a7394fee0d780371306b600bcd93c898f2419
Trevor Settles <roboteng@gmail.com>
2024-01-07 11:46:12 -0700

    read value of input on button click

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+function input_button() {
+    return document.getElementById("input-button");
+}
+
+function source_text() {
+    return document.getElementById("source-text").value;
+}
+
+input_button().addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(source_text());
+})
